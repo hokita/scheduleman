7a0d610872862786bc659e6d1c29eb59bf6d2400
hokita <hideee.0202@gmail.com>
2022-11-06 21:54:24 +0900

    First Commit.

---
+++ src/test/kotlin/dev/hokita/scheduleman/SchedulemanApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.hokita.scheduleman
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchedulemanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
